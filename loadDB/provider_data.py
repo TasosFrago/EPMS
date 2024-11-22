18321c90223d2241eadba65ea75dfabc4599d908
MyNameIsPAR <paraskevasjr3@gmail.com>
2024-11-22 18:48:58 +0200

    Submitting provider and month lists

--- loadDB/provider_data.py
+++ loadDB/provider_data.py
@@ -2,19 +2,75 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-
 class Provider_t(BaseModel):
-    element: int # attribute: type
-    optional_element: Optional[str]
-
+    name: str
+    phone: int
+    email: str
 
 providers: list[Provider_t] = [
     Provider_t(
-        element = 2, # example of provider
-        optional_element = None # optional attributes need to have value None
+        name = "ELPEDISON",
+        phone = 2181818128,
+        email = "customercare@elpedison.gr"
     ),
     Provider_t(
-        element = 1,
-        optional_element = "Test"
+        name = "EUNICE",
+        phone = 2103242020,
+        email = "info@eunice-group.com"
+    ),
+    Provider_t(
+        name = "NRG",
+        phone = 2188818101,
+        email = "cs@nrg.gr"
+    ),
+    Provider_t(
+        name = "OTE ESTATE",
+        phone = 2106372700,
+        email = "info@ote-estate.gr"
+    ),
+    Provider_t(
+        name = "PROTERGIA",
+        phone = 2103448500,
+        email = "info@prot-energy.gr"
+    ),
+    Provider_t(
+        name = "SOLAR ENERGY",
+        phone = 2385044506,
+        email = "supply@soumpasis.gr"
+    ),
+    Provider_t(
+        name = "VOLTERRA",
+        phone = 2130883000,
+        email = "customercare@volterra.gr"
+    ),
+    Provider_t(
+        name = "VOLTON",
+        phone = 2163001000,
+        email = "info@volton.gr"
+    ),
+    Provider_t(
+        name = "ΔΕΗ",
+        phone = 8009001000,
+        email = "info@dei.com.gr"
+    ),
+    Provider_t(
+        name = "ΕΛΙΝΟΙΛ",
+        phone = 2106241500,
+        email = "info@elin.gr"
+    ),
+    Provider_t(
+        name = "ΖΕΝΙΘ",
+        phone = 2311223099,
+        email = "info@zenith.gr"
+    ),
+    Provider_t(
+        name = "ΗΡΩΝ",
+        phone = 2130075499,
+        email = "customercare@heron.gr"
+    ),
+    Provider_t(
+        name = "ΦΥΣΙΚΟ ΑΕΡΙΟ ΕΛΛΗΝΙΚΗ ΕΤΑΙΡΙΑ ΕΝΕΡΓΕΙΑΣ",
+        phone = 2177181133,
+        email = "customerservice@fysikoaerioellados.gr"
     )
 ]
