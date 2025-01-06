13b65bc04507cb5a568b87bddc0a3c96329bd8be
MyNameIsPAR <paraskevasjr3@gmail.com>
2025-01-06 21:16:22 +0200

    Updated Schema and fixed ER_Diagram

--- db/schema.sql
+++ db/schema.sql
@@ -112,15 +112,108 @@ CREATE TABLE PAYS ( -- 8
 
 -- VIEW for calculating unpaid invoices
 CREATE VIEW INVOICE_PAYMENT_STATUS AS
-SELECT 
-    i.invoice_id,
-	i.receiver as consumer,
-    i.total AS invoice_total,
-    IFNULL(SUM(p.amount), 0) AS total_paid,
-    (IFNULL(SUM(p.amount), 0) >= i.total) AS is_paid
+SELECT i.invoice_id,
+	   i.receiver as consumer,
+       i.total AS invoice_total,
+       IFNULL(SUM(p.amount), 0) AS total_paid,
+       (IFNULL(SUM(p.amount), 0) >= i.total) AS is_paid
 FROM INVOICE i
 LEFT JOIN PAYS p
-ON
-    i.receiver = p.user AND i.provider = p.provider AND i.meter = p.supply_id
+ON i.receiver = p.user AND i.provider = p.provider AND i.meter = p.supply_id
 GROUP BY i.invoice_id, i.total
 ORDER BY i.invoice_id;
+
+-- VIEW for calculating average meter per employee for each region
+CREATE VIEW AVERAGE_METERS_PER_EMPLOYEE AS
+SELECT d.region AS department, 
+	   COUNT(m.supply_id) / (COUNT(DISTINCT e.badge))
+	   AS average_meters_per_employee
+FROM DEPARTMENT d
+LEFT OUTER JOIN EMPLOYEE e ON d.region = e.department
+LEFT OUTER JOIN METER m ON d.region = m.department
+GROUP BY d.region;
+
+-- Function to get meter's owner's phone number
+/*
+
+\d $$
+
+CREATE FUNCTION get_phone
+	(meter INT)
+RETURNS VARCHAR(10)
+BEGIN
+	DECLARE phone VARCHAR(10);
+	SET phone = (SELECT cell
+				 FROM CONSUMER, METER
+				 WHERE user_id = owner AND supply_id = meter);
+	RETURN phone;
+END$$
+
+\d ;
+
+*/
+
+--Function to find invoice date (DEPLOY AT YOUR OWN RISK)
+/*
+
+\d $$
+
+CREATE FUNCTION get_invoice_date
+	(invoiceID INT,
+	 invoice_plan INT,
+	 invoice_meter INT)
+RETURNS DATE
+BEGIN
+	DECLARE month_year DATE;
+	DECLARE month_year_string VARCHAR(30);
+	DECLARE month_offset INT;
+
+	SET month_offset = (SELECT COUNT(invoice_id)
+						FROM INVOICE
+						WHERE meter = invoice_meter
+							  AND plan = invoice_plan
+							  AND invoice_id < invoiceID);
+
+	SET month_year_string = (SELECT CONCAT("1-", month, "-", year)
+							 FROM PLAN
+							 WHERE plan_id = invoice_plan);
+
+	SET month_year = STR_TO_DATE(month_year_string, "%d-%M-%Y");
+
+	SET month_year = (DATE_ADD(month_year, INTERVAL month_offset MONTH));
+
+	RETURN month_year;
+END;
+
+\d ;
+
+*/
+
+-- Procedure to cut power
+/*
+
+\d $$
+
+CREATE PROCEDURE cut_power
+	(IN meter_id INT)
+BEGIN
+	DECLARE money_owed FLOAT;
+
+	SET money_owed = ((SELECT SUM(current_cost)
+					   FROM INVOICE
+					   WHERE meter = meter_id) -
+					  (SELECT SUM(amount)
+					   FROM PAYS
+					   WHERE supply_id = meter_id));
+
+	IF money_owed > 1500
+	THEN
+		UPDATE METER
+		SET status = 0
+		WHERE supply_id = meter_id;
+	END IF;
+END;
+
+\d ;
+
+*/
