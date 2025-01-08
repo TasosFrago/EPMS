3b3fc356550852b6b4009341f8e8fb9d3b594e71
TasosFrago <tasosfragkopoulos03@gmail.com>
2025-01-08 11:14:04 +0200

    Bug fix to frontend for compatibility with chrome

--- frontend/src/lib/components/MeterCard.svelte
+++ frontend/src/lib/components/MeterCard.svelte
@@ -41,46 +41,59 @@
 {/snippet}
 
 <div
-	class="custom-shadow mb-2 ml-1 mt-2 flex min-h-[350px] w-[225px] transform flex-col gap-6 rounded-2xl border-[2px] border-black px-3 py-3 transition-transform hover:-translate-x-1 hover:-translate-y-1 hover:shadow-black sm:max-w-[225px]"
+	class="custom-shadow mb-2 ml-1 mt-2 flex min-h-[350px] w-[225px] transform flex-col rounded-2xl border-[2px] border-black px-3 py-3 transition-transform hover:-translate-x-1 hover:-translate-y-1 hover:shadow-black sm:max-w-[225px]"
 >
-	<div class="top-header" role="button" tabindex="0" onclick={handleClick} onkeyup={handleClick}>
-		<ul>
-			<li class="text-md flex flex-row items-center text-[#FF0000]">
-				{@render PowerIcon()}
-				<span>{meterInfo.supply_id}</span>
-			</li>
-			<li class="text-black">{meterInfo.address}</li>
-		</ul>
-	</div>
 	<div
-		class="relative flex max-h-fit flex-grow flex-col justify-end"
+		class="top-header"
 		role="button"
 		tabindex="0"
 		onclick={handleClick}
 		onkeyup={handleClick}
+		title="invoice"
 	>
-		<div
-			class="middle absolute bottom-1/4 ml-1 w-full self-center border-l-[2px] border-l-black px-1 pr-2"
-		>
-			<ul>
-				<li class="text-md mb-3 font-bold">Invoice</li>
-				<li class="mb-1 flex flex-row justify-between text-sm">
-					<span>Expires:</span>
-					<span>{meterInfo.date ? meterInfo.date : '-'}</span>
-				</li>
-				<li class="flex flex-row justify-between text-sm">
-					<span>Amount:</span>
-					<span class="font-bold">{meterInfo.amount ? meterInfo.amount + '€' : '-'}</span>
-				</li>
-			</ul>
+		<ul>
+			<li
+				class="text-md flex flex-row items-center text-[#FF0000] underline-offset-4 hover:underline"
+			>
+				{@render PowerIcon()}
+				<span>{meterInfo.supply_id}</span>
+			</li>
+			<li class="text-black underline-offset-4 hover:underline">{meterInfo.address}</li>
+		</ul>
+	</div>
+	<div
+		class="relative h-full w-full"
+		role="button"
+		tabindex="0"
+		onclick={handleClick}
+		onkeyup={handleClick}
+		title="invoice"
+	>
+		<div class="absolute bottom-12 w-full">
+			<div class=" flex max-h-fit flex-col justify-end px-1">
+				<div class="middle w-full border-l-[2px] border-l-black px-1">
+					<ul>
+						<li class="text-md mb-3 font-bold">Invoice</li>
+						<li class="mb-1 flex flex-row justify-between text-sm">
+							<span>Expires:</span>
+							<span>{meterInfo.date ? meterInfo.date : '-'}</span>
+						</li>
+						<li class="flex flex-row justify-between text-sm">
+							<span>Amount:</span>
+							<span class="font-bold">{meterInfo.amount ? meterInfo.amount + '€' : '-'}</span>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
-	<div class="bottom-footer mx-1">
+	<div class="bottom-footer mx-1 w-full self-center">
 		<!-- FIX: Add pay redirect path -->
 		<button
 			class="w-full items-center rounded-xl p-1 font-bold text-white"
 			style="background: {meterInfo.is_paid ? 'green' : 'black'}"
 			onclick={OnClickF}
+			title="pay"
 		>
 			{meterInfo.is_paid == null ? '-' : meterInfo.is_paid ? 'Paid' : 'Pay'}
 		</button>
